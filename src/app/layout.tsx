7b50ce3a954df6aa280f3bf939da5019c77f053d
Rick Huang <rick.liyue.huang@gmail.com>
2023-08-13 23:14:39 +1000

    feat: create navbar and show it on root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,8 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export async function Navbar() {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+      <div className="container max-w-7xl h-full flex items-center justify-between gap-2">
+        {/* Logo */}
+        <Link href={"/"} className="flex gap-2 items-center">
+          {/* <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" /> */}
+
+          <div className="w-8 h-8 sm:h-6 sm:w-6 leading-8 sm:leading-6">
+            <img src="logo.png" />
+          </div>
+          {/* <Image src={"/logo.png"} width={32} height={32} alt="logo" /> */}
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Blog
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+}
